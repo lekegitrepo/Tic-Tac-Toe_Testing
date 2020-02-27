45515b65ab57112965728bdc51108d3e9b4207a7
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-27 14:09:54 -0600

    add reset game test

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -7,9 +7,23 @@ const gameBoard = (() => {
     }
   };
 
+  const checkRows = () => {
+    if (board[0] === board[1] && board[0] === board[2]) {
+      return board[0];
+    } if (board[3] === board[4] && board[3] === board[5]) {
+      return board[3];
+    } if (board[6] === board[7] && board[6] === board[8]) {
+      return board[6];
+    }
+  };
+
+  const resetBoard = () => (board['', '', '', '', '', '', '', '', '']);
+
   return {
     board,
     setBoardTile,
+    checkRows,
+    resetBoard,
   };
 })();
 
--- tic-tac-toe/tic-tac-toe.test.js
+++ tic-tac-toe/tic-tac-toe.test.js
@@ -1,5 +1,9 @@
 const { gameBoard } = require('./tic-tac-toe');
 
-it('game board', () => {
+it('setBoardTile function set tile position on game board', () => {
   expect(gameBoard.setBoardTile(0, 'X')).toBe(gameBoard.board['X', '', '', '', '', '', '', '', '']);
+});
+
+it('reset game board', () => {
+  expect(gameBoard.resetBoard()).toBe(gameBoard.board['', '', '', '', '', '', '', '', '']);
 });
