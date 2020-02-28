7ac39bf0cc9eb0106e07d707efd424a5de4b87dc
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-28 11:14:21 -0600

    improve the logic to check for row win pattern

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,26 +1,32 @@
+/* eslint-disable prefer-destructuring */
 const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
+  const arrayTiles = ['', '', '', '', '', '', '', '', ''];
 
   const setBoardTile = (index, value) => {
-    if (board[index] == '') {
-      board[index] = value;
+    if (arrayTiles[index] == '') {
+      arrayTiles[index] = value;
     }
   };
 
-  const checkRows = () => {
-    if (board[0] === board[1] && board[0] === board[2]) {
-      return board[0];
-    } if (board[3] === board[4] && board[3] === board[5]) {
-      return board[3];
-    } if (board[6] === board[7] && board[6] === board[8]) {
-      return board[6];
+  const checkRows = (board = arrayTiles) => {
+    let rows = false;
+    if ((board[0] !== '')
+      && (board[0] === board[1] && board[0] === board[2])) {
+      rows = board[0];
+    } else if ((board[3] !== '')
+      && (board[3] === board[4] && board[3] === board[5])) {
+      rows = board[3];
+    } else if ((board[6] !== '')
+      && (board[6] === board[7] && board[6] === board[8])) {
+      rows = board[6];
     }
+    return rows;
   };
 
-  const resetBoard = () => (board['', '', '', '', '', '', '', '', '']);
+  const resetBoard = () => (arrayTiles['', '', '', '', '', '', '', '', '']);
 
   return {
-    board,
+    arrayTiles,
     setBoardTile,
     checkRows,
     resetBoard,
